d514c0ba42519b6e13ef24dff4cc157fa255ade9
suhailpalazhi <mhdsuhailplayi@gmail.com>
2026-05-05 23:45:25 +0530

    Increased ImageField max_length to fix Pronto URL crashes

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True, max_length=500)
     category = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     bio = models.TextField(blank=True)
-    avatar = models.ImageField(upload_to='avatars/', default='default.png')
+    avatar = models.ImageField(upload_to='avatars/', default='default.png', max_length=500)
 
     def __str__(self):
         return self.username
