1d77d7a35f0eea989a65cc78298ce02c612fb873
suhailpalazhi <mhdsuhailplayi@gmail.com>
2026-05-05 21:57:06 +0530

    Fixed duplicate email registration and avatar upload

--- users/serializers.py
+++ users/serializers.py
@@ -9,15 +9,27 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'bio', 'avatar']
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            password=validated_data['password']
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            bio=validated_data.get('bio', '')
         )
+        if 'avatar' in validated_data:
+            user.avatar = validated_data['avatar']
+            user.save()
         return user
